2824c55a00e87c13c03c133f36839b2a60d1431c
mkarim-rtx <149624049+mkarim-rtx@users.noreply.github.com>
2026-05-19 17:33:09 -0700

    Add EWTS support to SFT Unit Test (#20)
    * Remove using generic EWTS_ID. Define LOG in submodule instead of relying on generic definition in ewts library.
    
    * updated for ewts
    
    ---------
    
    Co-authored-by: Carolyn.Maynard <carolyn.maynard@rtx.com>

--- tests/run_unittest.sh
+++ tests/run_unittest.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 set -e
 
+EWTS_PREFIX="${EWTS_PREFIX:-/tmp/ewts_install}"
+EWTS_LIB_DIR="${EWTS_LIB_DIR:-${EWTS_PREFIX}/lib}"
+
+if [ ! -d "${EWTS_LIB_DIR}" ] && [ -d "${EWTS_PREFIX}/lib64" ]; then
+  EWTS_LIB_DIR="${EWTS_PREFIX}/lib64"
+fi
+
 ${CXX:-g++} -std=c++17 -Wall -O -g \
   ./main_unittest.cxx \
   ../src/bmi_soil_freeze_thaw.cxx \
   ../src/soil_freeze_thaw.cxx \
-  ../src/Logger.cpp \
   -I../include \
   -I../bmi \
+  -I"${EWTS_PREFIX}/include" \
+  -L"${EWTS_LIB_DIR}" \
+  -Wl,-rpath,"${EWTS_LIB_DIR}" \
+  -lewts_cpp \
   -lboost_serialization \
   -lm \
   -o run_sft
